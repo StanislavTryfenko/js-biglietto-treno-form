3efcc9707a4c716aa5779ecfbb395806cfbb9328
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-02-21 03:59:05 +0100

    Consegna

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,37 +12,40 @@ let priceSeniorTag = ("Biglietto Over 65");
 let priceStandardTag = ("Biglietto Standard");
 
 function addTicket() {
-    let nameSurname = document.getElementById("nameSurname");  
+    let nameSurname = document.getElementById("nameSurname").value;  
     let km = document.getElementById("km").value;  
-    let age = document.getElementById("age");
+    let age = document.getElementById("age").value;
 
     let table = document.getElementById("listTickets");
     let newRow = table.insertRow();
+
     let cell1 = newRow.insertCell(0);
     let cell2 = newRow.insertCell(1);
     let cell3 = newRow.insertCell(2);
     let cell4 = newRow.insertCell(3);
     let cell5 = newRow.insertCell(4);
+    let cell6 = newRow.insertCell(5);
     
     let ticketPrice;
-    if(age.value === "Under 18") {
+    if(age === "Under 18") {
         ticketPrice = Number((km) * (price_km) * Number(discountJunior));
     }
-    else if(age.value === "Over 65") {
+    else if(age === "Over 65") {
         ticketPrice = Number(Number(km) * Number(price_km) * Number(discountSenior));
     }
-    else if (age.value === "Standard") {
+    else if (age === "Standard") {
         ticketPrice = Number(Number(km) * Number(price_km));
     }
 
     let trainCarriage = (Math.floor(Math.random() * 10) + 1);
     let codeTrip = (Math.floor(Math.random() * 10000) + 1);
 
-    cell1.innerHTML = (nameSurname.value);
-    cell2.innerHTML = (age.value);
-    cell3.innerHTML = (trainCarriage);
-    cell4.innerHTML = (codeTrip);
-    cell5.innerHTML = (ticketPrice);
+    cell1.innerHTML = nameSurname;
+    cell2.innerHTML = age;
+    cell3.innerHTML = trainCarriage;
+    cell4.innerHTML = codeTrip;
+    cell5.innerHTML = km;
+    cell6.innerHTML = ticketPrice;
 }
 
 
